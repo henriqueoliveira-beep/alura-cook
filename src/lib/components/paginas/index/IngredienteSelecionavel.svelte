3728e486ba25ea518c08322434a568fc0152e480
Henrique <henrique.oliveira@mv.com.br>
2026-05-25 10:14:23 -0300

    projeto inicial

---
+++ src/lib/components/paginas/index/IngredienteSelecionavel.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { minhaLista } from "$lib/stores/minhaLista";
+    import Tag from "../../compartilhados/Tag.svelte";
+
+    export let ingrediente: string;
+    $: selecionado = $minhaLista.includes(ingrediente);
+
+    function onClick() {
+        if (!selecionado) {
+            minhaLista.adicionarIngrediente(ingrediente);
+        } else {
+            minhaLista.removerIngrediente(ingrediente);
+        }
+    }
+</script>
+
+<button on:click={onClick}>
+    <Tag ativa={selecionado}>{ingrediente}</Tag>
+</button>
+
+
+<style>
+    button {
+        cursor: pointer;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import "../app.css";
+    import Cabecalho from "$components/paginas/layout/Cabecalho.svelte";
+    import MinhaLista from "$components/paginas/layout/MinhaLista.svelte";
+    import Rodape from "$components/paginas/layout/Rodape.svelte";
+    import { minhaLista } from "$lib/stores/minhaLista";
+</script>
+
+<div class="container-principal">
+    <Cabecalho />
+    <div class="estilo-principal">
+        {#if $minhaLista.length}
+            <div class="minha-lista-container">
+                <MinhaLista />
+                <div class="divisoria"></div>
+            </div>
+        {/if}
+
+        <slot></slot>
+    </div>
+    <Rodape />
+</div>
+
+<style>
+    .minha-lista-container {
+        margin-bottom: 2rem;
+    }
+
+    .divisoria {
+        width: 40vw;
+        height: 2px;
+        background-color: var(--verde);
+        margin: 0 auto;
+    }
+
+    .container-principal {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    .estilo-principal {
+        text-align: center;
+        padding: 0 5vw 3.375rem;
+        flex: 1;
+    }
+</style>
